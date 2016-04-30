62116092dbce240d197adba5ee848c47a31a1299
Marc Domínguez de la Rocha <marcdominguezr@gmail.com>
2016-04-30 17:34:58 +0200

    Added files via upload
    Añadido ampliar_palabra();

--- Palabra.hh
+++ Palabra.hh
@@ -27,5 +27,10 @@ class Palabra {
     /* Pre: Cierto */
     /* Post: Devuelve true si el p.i tiene un signo de      */
     /* puntuacion pegado al final, en caso contrario, false */
-  
+    
+    void ampliar_palabra(char c);
+    /* Pre: El p.i es un identificador de cita = ID          */
+    /* Post: ID = ID + la concatenacion del caracter c       */
+    /* es decir, ID.size() += 1, donde ID[ID.size() - 1] = c */
+    /* obteniendo: [0...n - 1, c]... */    
 }
