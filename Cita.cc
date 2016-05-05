46f9e6e65d4d0ed1e7ed939353752d4d4efe9317
Marc Domínguez de la Rocha <marcdominguezr@gmail.com>
2016-05-05 12:13:51 +0200

    Added files via upload
    Subida una implementación incompleta que únicamente contiene la creadora de la clase Cita.

---
+++ Cita.cc
@@ -0,0 +1,13 @@
+#include "Cita.hh"
+
+Cita::Cita(vector<Frase> contenido, string titulo_texto, string autor_texto, int x, int y, string identificador){
+  this->contenido = contenido;
+  this->titulo_texto = titulo_texto;
+  this->autor_texto = autor_texto;
+  this->x = x;
+  this->y = y;
+}
+
+void Cita::info(){
+  
+}
