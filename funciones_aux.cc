fbc1886cde360ddc9cb478dde1cc380d89075962
Marc Domínguez de la Rocha <marcdominguezr@gmail.com>
2016-05-28 18:50:42 +0200

    Add files via upload

---
+++ funciones_aux.cc
@@ -0,0 +1,22 @@
+#include "funciones_aux.hh"
+
+void recorta(istringstream& iss){ //iss es un parametro de salida
+  int pos = 0;
+  bool encontrado = false;
+  string aux = iss.str();
+  while(aux[pos] == ' ') ++pos; //Llegamos hasta el primer espacio
+  while(aux[pos] != ' ' and pos < aux.length()) ++pos; //Numero de posiciones hasta el espacio
+  aux.erase(0, pos + 1); //Contamos el ultimo espacio tambien
+  iss.str(aux); //Convertimos la string aux en un iss
+}
+
+void refina_pf(string& palabra){ //Refina principio y final
+  palabra = palabra.substr(1, palabra.length() - 2);
+}
+
+void refina_signo(string& palabra){
+  char c = palabra [palabra.length() - 1];
+  if(not ((c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z'))) palabra = palabra.substr(0, palabra.length() - 1);
+}
+
+
---
+++ funciones_aux.hh
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+
+using namespace std;
+
+/** @brief Elimina la primera parte de un sstream
+ *  \pre cierto
+ *  \post se ha eliminado la primera palabra 
+ *  antes del primer espacio de <b>iss</b>
+ */
+void recorta(istringstream& iss);
+
+/** @brief Elimina el primer y ultimo caracter
+ * \pre cierto
+ * \post se han eliminado el primer y 
+ * ultimo caracter de la cadena
+ */
+void refina_pf(string& palabra);
+
+/** @brief Elimina el signo de una palabra
+ *  \pre cierto
+ *  \post si tenia un signo al final
+ *  de la palabra este ha sido eliminado,
+ *  en caso contrario no se modifica
+ */
+void refina_signo(string& palabra);
